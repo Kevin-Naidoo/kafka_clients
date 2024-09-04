ebad51847367d9f8f98225e2b8787a79bbb3e890
Kevin-Naidoo <kevinnelsonnaidoo@gmail.com>
2024-09-04 16:23:12 +0200

    Added ips of 3 node kafka cluster

--- kafka_producer/config/config.exs
+++ kafka_producer/config/config.exs
@@ -3,7 +3,8 @@ import Config
 config :brod,
   clients: [
     kafka_client: [
-      endpoints: ['10.10.10.120': 9092],
+      #kafka1 , kafka2 , kafka3
+      endpoints: ['10.10.10.129': 9092, '10.10.10.127': 9092, '10.10.10.120': 9092],
       auto_start_producers: true,
       # The following :ssl and :sasl configs are not
       # required when running kafka locally unauthenticated
